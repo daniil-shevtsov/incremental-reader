c236711a6027e50997f806024ffa317a17176afe
Daniil Shevtsov <me@daniilshevtsov.com>
2020-12-05 00:04:15 +0300

    Create Learning Fragment

---
+++ app/src/main/java/shevtsov/daniil/incrementalreader/learning/LearningFragment.kt
@@ -0,0 +1,12 @@
+package shevtsov.daniil.incrementalreader.learning
+
+import androidx.fragment.app.Fragment
+import shevtsov.daniil.incrementalreader.R
+import shevtsov.daniil.incrementalreader.core.util.viewLifecycleLazy
+import shevtsov.daniil.incrementalreader.databinding.FragmentCreationBinding.bind
+
+class LearningFragment : Fragment(R.layout.fragment_learning) {
+
+    private val binding by viewLifecycleLazy { bind(requireView()) }
+
+}
