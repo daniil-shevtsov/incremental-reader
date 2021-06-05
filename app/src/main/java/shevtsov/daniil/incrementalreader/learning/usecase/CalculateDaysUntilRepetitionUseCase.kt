b1c936599974bd4465253bd9176d09dcb72dc594
Daniil Shevtsov <me@daniilshevtsov.com>
2021-06-05 23:56:26 +0300

    findLastGroup test passes

--- app/src/main/java/shevtsov/daniil/incrementalreader/learning/usecase/CalculateDaysUntilRepetitionUseCase.kt
+++ app/src/main/java/shevtsov/daniil/incrementalreader/learning/usecase/CalculateDaysUntilRepetitionUseCase.kt
@@ -6,7 +6,36 @@ import javax.inject.Inject
 class CalculateDaysUntilRepetitionUseCase @Inject constructor() {
 
     operator fun invoke(scores: List<ScoreValue>): Double {
+        val correctAnswerStreak = findAnswerStreak(scores)
         return 0.0
+    }
+
+    fun findAnswerStreak(scores: List<ScoreValue>): Int {
+        return scores.formGroups { score -> score.value >= MinimumCorrect }
+            .maxByOrNull { group -> group.count() }
+            ?.count() ?: 0
+    }
+
+    fun <T> List<T>.formGroups(
+        predicate: (value: T) -> Boolean
+    ): List<List<T>> {
+        val groups = mutableListOf<MutableList<T>>()
+        forEach { value ->
+            val lastGroup = groups.findLastGroup(predicate) as MutableList<T>?
+
+            val lastValue = lastGroup
+                ?.lastOrNull { groupValue -> predicate.invoke(groupValue) }
+
+            if (predicate.invoke(value)) {
+                if (lastValue != null && predicate.invoke(lastValue)) {
+                    lastGroup?.add(value)
+                } else {
+                    groups.add(mutableListOf(value))
+                }
+
+            }
+        }
+        return groups
     }
 
     private companion object {
@@ -17,6 +46,11 @@ class CalculateDaysUntilRepetitionUseCase @Inject constructor() {
             val d = 0.02
             val omega = 0.2
         }
+
+        val MinimumCorrect = 3
     }
 
 }
+
+fun <T> List<List<T>>.findLastGroup(predicate: (value: T) -> Boolean) =
+    lastOrNull { group -> group.any { groupValue -> predicate.invoke(groupValue) } }
--- app/src/test/java/shevtsov/daniil/incrementalreader/learning/usecase/CalculateDaysUntilRepetitionUseCaseTest.kt
+++ app/src/test/java/shevtsov/daniil/incrementalreader/learning/usecase/CalculateDaysUntilRepetitionUseCaseTest.kt
@@ -26,6 +26,50 @@ internal class CalculateDaysUntilRepetitionUseCaseTest {
         assertEquals(expectedDays, days, DELTA)
     }
 
+    @Test
+    fun `when the same score consecutive streak - then returns correct streak`() {
+        val scores = listOf(0, 0, 0, 1, 3, 3, 3, 0, 4, 4, 4, 4).toScores()
+        val expectedStreak = 4
+
+        val streak = usecase.findAnswerStreak(scores)
+
+        assertEquals(expectedStreak, streak)
+    }
+
+    @Test
+    fun `when the numbers are not consecutive - then doesn't count as streak`() {
+        val scores = listOf(3, 3, 3, 0, 3, 3, 3, 0, 4, 4, 4, 4).toScores()
+        val expectedStreak = 4
+
+        val streak = usecase.findAnswerStreak(scores)
+
+        assertEquals(expectedStreak, streak)
+    }
+
+    @Test
+    fun `when the different score consecutive streak - then returns correct streak`() {
+        val scores = listOf(0, 0, 0, 1, 3, 3, 3, 4, 5, 4, 3, 0, 4, 4, 4, 4).toScores()
+        val expectedStreak = 7
+
+        val streak = usecase.findAnswerStreak(scores)
+
+        assertEquals(expectedStreak, streak)
+    }
+
+    @Test
+    fun `when find last group - last group found`() {
+        val expectedGroup = mutableListOf(5, -1, -2)
+        val groups: MutableList<MutableList<Int>> = mutableListOf<MutableList<Int>>(
+            mutableListOf(3, -1, 2),
+            expectedGroup,
+            mutableListOf(1, 2, 3),
+        )
+
+        val group = groups.findLastGroup { it < 0}
+
+        assertEquals(expectedGroup, group)
+    }
+
     private fun List<Int>.toScores() = map { ScoreValue(it.toLong()) }
 
     private companion object {
